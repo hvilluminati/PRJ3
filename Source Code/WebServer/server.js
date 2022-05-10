5fdeec612fb41f8882d8c17e9cf988231aab1df2
lassehj0 <100580392+lassehj0@users.noreply.github.com>
2022-05-10 15:56:24 +0200

    Update server.js

--- Source Code/WebServer/server.js
+++ Source Code/WebServer/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+var JsonDB = require('node-json-db').JsonDB;
+const Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
 
+var db = new JsonDB(new Config("myDataBase", true, false, "/"));
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
+app.use(express.urlencoded());
 
-app.get('/', function(req, res, next) {  
+app.get('/', function(req, res, next) {
+    res.sendFile("/index.html", function(err) {
+        if (err) {
+            next(err)
+        } else {
+            console.log('Sent:', "index.html")
+        }
+    })
+})
+
+app.post('/', function(request, response){
+    console.log(request.body.amount);
+    console.log(request.body.size);
+});
+
+app.get('/', function(req, res, next) {
     res.sendFile("/index.html", function(err) {
         if (err) {
             next(err)
